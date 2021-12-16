e7d73b03b5f2347a1a11e9b1d8e3866b722fd262
quntnim <genshin_imp@dgsw.hs.kr>
2021-12-16 09:54:14 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+console.log("Hello World!")
+const num=10 //상수
+let num3=303 // 변수 1
+var num2=20.2 //변수 2 잘안쓴데요
+
+const con: string = "asdf" //타입 string int 직접만듦
+
+type myType = { // 타입 만들기
+    name: string,
+    age: number
+}
+
+const me: myType = {
+    name: '박다윤',
+    age: 17
+}
+
+console.log(me)
